f127aeb1a29552f570e5845ba0ec888a4fd4cb5b
TwistyTea <Adinathuparkar23@gmail.com>
2022-10-10 14:07:06 +0530

    add map control

--- public/js/map.js
+++ public/js/map.js
@@ -7,6 +7,9 @@ const map = new mapboxgl.Map({
     zoom: 6, // starting zoom
     projection: "globe", // display the map as a 3D globe
 });
+
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-left')
+
 map.on("style.load", () => {
     map.setFog({}); // Set the default atmosphere style
 });
